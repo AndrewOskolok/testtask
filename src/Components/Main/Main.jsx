f3f2f2ab60fae9af0e8e1949c0b0f7f9a5163124
Oscolok <oscolok7@gmail.com>
2020-11-05 15:39:11 +0200

    optimize

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,14 +1,11 @@
-import React, { Suspense } from "react";
+import React from "react";
 import TrackVisibility from "react-on-screen";
 
 import Home from "../Home/Home";
 import AboutMe from "../AboutMe/AboutMe";
-// import Users from "../Users/Users";
-// import Register from "../Register/Register";
+import Users from "../Users/Users";
+import Register from "../Register/Register";
 import "./Main.scss";
-
-const Users = React.lazy(() => import("../Users/Users"));
-const Register = React.lazy(() => import("../Register/Register"));
 
 const Main = () => {
   return (
@@ -16,15 +13,13 @@ const Main = () => {
       <Home />
       <AboutMe />
 
-      <Suspense fallback={null}>
-        <TrackVisibility once offset={200}>
-          {({ isVisible }) => isVisible && <Users />}
-        </TrackVisibility>
+      <TrackVisibility once offset={200}>
+        {({ isVisible }) => isVisible && <Users />}
+      </TrackVisibility>
 
-        <TrackVisibility once offset={200}>
-          {({ isVisible }) => isVisible && <Register />}
-        </TrackVisibility>
-      </Suspense>
+      <TrackVisibility once offset={200}>
+        {({ isVisible }) => isVisible && <Register />}
+      </TrackVisibility>
     </main>
   );
 };
