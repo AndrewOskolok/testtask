6fa9925d142ad86f9b344ae869f4ff252c985c29
Oscolok <oscolok7@gmail.com>
2020-11-05 15:14:35 +0200

    optimize

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,24 +8,24 @@ import ModalMenu from "../Components/ModalMenu/ModalMenu";
 import { setToken } from "../redux/actions/tokenAction";
 import "./App.scss";
 
-axios.defaults.baseURL =
-  "https://frontend-test-assignment-api.abz.agency/api/v1";
+// axios.defaults.baseURL =
+//   "https://frontend-test-assignment-api.abz.agency/api/v1";
 
 function App() {
-  const dispatch = useDispatch();
+  //   const dispatch = useDispatch();
 
-  const getToken = async (page) => {
-    try {
-      const request = await axios.get(`/token`);
-      dispatch(setToken(request.data.token));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //   const getToken = async (page) => {
+  //     try {
+  //       const request = await axios.get(`/token`);
+  //       dispatch(setToken(request.data.token));
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
 
-  useEffect(() => {
-    getToken();
-  }, []);
+  // useEffect(() => {
+  //   getToken();
+  // }, []);
 
   return (
     <div className="app">
