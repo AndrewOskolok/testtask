2a33eed83282a0173be18510f3ae5b849eb0dd1f
Oscolok <oscolok7@gmail.com>
2020-11-05 14:27:36 +0200

    add lazy load

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,17 +1,25 @@
-import React from "react";
-import Home from "../Home/Home";
-import AboutMe from "../AboutMe/AboutMe";
-import Users from "../Users/Users";
-import Register from "../Register/Register";
-import "./Main.scss";
+import React, { Suspense } from "react";
+
+const Home = React.lazy(() => import("../Home/Home"));
+const AboutMe = React.lazy(() => import("../AboutMe/AboutMe"));
+const Users = React.lazy(() => import("../Users/Users"));
+const Register = React.lazy(() => import("../Register/Register"));
+
+// import Home from "../Home/Home";
+// import AboutMe from "../AboutMe/AboutMe";
+// import Users from "../Users/Users";
+// import Register from "../Register/Register";
+// import "./Main.scss";
 
 const Main = () => {
   return (
     <main>
-      <Home />
-      <AboutMe />
-      <Users />
-      <Register />
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Home />
+        <AboutMe />
+        <Users />
+        <Register />
+      </Suspense>
     </main>
   );
 };
