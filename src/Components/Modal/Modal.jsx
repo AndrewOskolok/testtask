2101b47d28e2ef8d91ee7b5455739d0a66a09ca7
Oscolok <oscolok7@gmail.com>
2020-11-06 02:39:30 +0200

    delete unused string

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CSSTransition } from "react-transition-group";
 import { setModal } from "../../redux/actions/modalAction";
